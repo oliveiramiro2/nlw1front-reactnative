1a6d6db31a9293a50a07cf0b3fea8a21944e2b7b
oliveiramiro2 <ramiromares95@gmail.com>
2023-01-17 00:07:58 -0300

    added fonts to app

---
+++ src/components/Loading.tsx
@@ -0,0 +1,14 @@
+import { ActivityIndicator, View } from "react-native";
+
+export default function Loading() {
+  return (
+    <View style={{ 
+      flex: 1, 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      backgroundColor: "#09090a"
+    }}>
+      <ActivityIndicator color="#7c3aed" />
+    </View>
+  )
+}
